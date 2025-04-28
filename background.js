42004bd33b070acdbfe61b2a03659139158f3ed8
Diego Efren Elizalde Barboza <potenciapanadera@gmail.com>
2025-04-27 20:41:00 -0600

    Back to not working again

--- background.js
+++ background.js
@@ -1,14 +1,25 @@
 chrome.commands.onCommand.addListener((command) => {
   if (command === 'open_modal') {
-    chrome.windows.create({
-      url: 'index.html',
+    chrome.system.display.getInfo((displays) => {
+      const primaryDisplay = displays.find(display => display.isPrimary);
+      if (primaryDisplay) {
+        const screenWidth = primaryDisplay.workArea.width;
+        const screenHeight = primaryDisplay.workArea.height;
+        const modalWidth = 400;  // Modal width
+        const modalHeight = 300; // Modal height
+        const left = Math.round((screenWidth - modalWidth) / 2);
+        const top = Math.round((screenHeight - modalHeight) / 2) - 50; // Add top padding
 
-      type: 'popup',
-      width: 400,
 
-      height: 300,
-      left: Math.round((window.screen.width - 400) / 2),
-      top: Math.round((window.screen.height - 300) / 2) - 50
+        chrome.windows.create({
+          url: 'index.html', // The file to open
+          type: 'popup', // Makes it a popup window
+          width: modalWidth, // Width of the modal
+          height: modalHeight, // Height of the modal
+          left: left, // Centers it horizontally
+          top: top // Centers it vertically with padding
+        });
+      }
     });
   }
 });
