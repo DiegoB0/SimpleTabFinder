f0a22339607168442d82df6c5c0500bff0774ebf
Diego Efren Elizalde Barboza <potenciapanadera@gmail.com>
2025-04-27 20:33:00 -0600

    I am loosing my mind over a fucking simple extension

--- background.js
+++ background.js
@@ -1,30 +1,8 @@
 chrome.commands.onCommand.addListener((command) => {
-  if (command === "open_modal") {
-    chrome.scripting.executeScript({
-      target: { tabId: chrome.tabs.query({ active: true, currentWindow: true })[0].id },
-      func: openModal
-    });
-
+  if (command === 'open_modal') {
+    chrome.action.openPopup(); // Opens the popup (modal)
   }
+
 });
 
-function openModal() {
-  const modal = document.createElement('div');
-  modal.style.position = 'fixed';
-  modal.style.top = '50%';
-  modal.style.left = '50%';
-  modal.style.transform = 'translate(-50%, -50%)';
-  modal.style.padding = '20px';
-  modal.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
-  modal.style.color = 'white';
-  modal.style.borderRadius = '8px';
-  modal.innerText = 'It works!';
-
-
-  document.body.appendChild(modal);
-
-  setTimeout(() => {
-    modal.remove();
-  }, 3000);
-}
 
