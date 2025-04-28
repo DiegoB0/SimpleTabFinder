d205d27682a37963799011dc0c2d7f17a01bf5cc
Diego Efren Elizalde Barboza <potenciapanadera@gmail.com>
2025-04-27 20:37:36 -0600

    Finally opens the modal

--- background.js
+++ background.js
@@ -1,8 +1,15 @@
 chrome.commands.onCommand.addListener((command) => {
   if (command === 'open_modal') {
-    chrome.action.openPopup(); // Opens the popup (modal)
+    chrome.windows.create({
+      url: 'index.html',
+
+      type: 'popup',
+      width: 400,
+
+      height: 300,
+      left: Math.round((window.screen.width - 400) / 2),
+      top: Math.round((window.screen.height - 300) / 2) - 50
+    });
   }
-
 });
-
 
